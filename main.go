2aa7259b77ba1db28086b8025f5ea2b8ec84d607
bmcculley <bmcculley@ejank.com>
2021-03-25 19:32:19 -0400

    update handler/live updates
    Updated the handler to change the way the reverse proxy URL is
    handled. This enables the ability to update the URL on the fly.
    
    Also added devMode to ease local development.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import(
         "encoding/json"
         "fmt"
         "log"
+        "net"
         "net/url"
         "net/http"
         "net/http/httputil"
@@ -15,74 +16,113 @@ import(
 var (
         allowedIP string
         revProxURL string
-        listenPort string
+        listenHostPort string
 
-        quit = make(chan bool)
         liveReload = false
+        devMode = false
+
+        hostProxy = make(map[string]*httputil.ReverseProxy)
 )
 
+type baseHandle struct{}
+
 func headerToJson(header http.Header) {
+
         jsonHeader, err := json.Marshal(header)
         if err != nil {
                 fmt.Println(err)
         }
+
         log.Println(string(jsonHeader))
+
 }
 
 func startRevProx() {
-        remote, err := url.Parse(revProxURL)
-        if err != nil {
-                panic(err)
-        } 
+        
+        h := &baseHandle{}
+        http.Handle("/", h)
 
-        proxy := httputil.NewSingleHostReverseProxy(remote)
-        http.HandleFunc("/", handler(proxy))
-        log.Printf("Starting server listening on %s", listenPort)
-        err = http.ListenAndServe(listenPort, nil)
+        server := &http.Server {
+                Addr: listenHostPort,
+                Handler: h,
+        }
+
+        log.Printf("Starting server listening on %s", listenHostPort)
+
+        err := server.ListenAndServe()
+
         if err != nil {
                 panic(err)
         }
 }
 
-func handler(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
-        return func(w http.ResponseWriter, r *http.Request) {
-                rip := r.Header.Get("X-Forwarded-For")
-                r.Header.Add("URL", r.URL.String())
-                headerToJson(r.Header)
+func (h *baseHandle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-                if rip != allowedIP {
-                        http.Error(w, "Not authorized", http.StatusUnauthorized)
-                } else {
-                        w.Header().Set("X-Server", "revprox")
-                        p.ServeHTTP(w, r)
+        rip := r.Header.Get("X-Forwarded-For")
+        if rip == "" {
+                var err error 
+                rip, _, err = net.SplitHostPort(r.RemoteAddr)
+                if err != nil {
+                        log.Println(err)
                 }
         }
+
+        r.Header.Add("URL", r.URL.String())
+        headerToJson(r.Header)
+
+        if rip == allowedIP || devMode {
+                remoteURL, err := url.Parse(revProxURL)
+                if err != nil {
+                        log.Println(err)
+                }
+                proxy := httputil.NewSingleHostReverseProxy(remoteURL)
+
+                w.Header().Set("X-Server", "revprox")
+                proxy.ServeHTTP(w, r)
+
+        } else {
+
+                http.Error(w, "Not authorized", http.StatusUnauthorized)
+
+        }
+        
 }
 
 func loadConfig() {
+
         viper.SetConfigName("config")
         viper.SetConfigType("yaml")
         viper.AddConfigPath("/etc/revprox/")
         viper.AddConfigPath(".") 
+        
         err := viper.ReadInConfig()
         if err != nil {
                 panic(fmt.Errorf("Fatal error config file: %s \n", err))
         }
+
         allowedIP = viper.GetString("allowedIP")
         revProxURL = viper.GetString("revProxURL")
-        listenPort = viper.GetString("listenPort")
+        listenHostPort = viper.GetString("listenHostPort")
         liveReload = viper.GetBool("liveReload")
+        devMode = viper.GetBool("devMode")
 
         if liveReload {
+        
                 viper.WatchConfig()
                 viper.OnConfigChange(func(e fsnotify.Event) {
                         log.Println("Config file changed:", e.Name)
                         allowedIP = viper.GetString("allowedIP")
+                        revProxURL = viper.GetString("revProxURL")
+                        devMode = viper.GetBool("devMode")
                 })
+        
         }
+        
 }
 
 func main() {
+
         loadConfig()
         startRevProx()
+
 }
